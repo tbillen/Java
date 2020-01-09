51c78bcbdec1658aafa914bc2284429f596711fc
tobias.billen <31624463+Naxon89@users.noreply.github.com>
2020-01-09 16:30:06 +0100

    Dies ist ein Git Projekt

---
+++ src/testgit/TestGit.java
@@ -0,0 +1,14 @@
+package testgit;
+
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Hello World");
+
+    }
+    
+}
